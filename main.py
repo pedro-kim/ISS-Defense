20e32510b7e6f69a8c592a334c6840d497a0f2b8
Pedro Henrique Ferron Kim <113215050+pedro-kim@users.noreply.github.com>
2023-03-26 14:49:09 -0300

    Add main.py via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+
+WIDTH, HEIGHT = 540, 960
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("ISS Defense")
+
+FPS = 60
+
+BACKGROUND_IMAGE = pygame.image.load("Images/background.png")
+
+WIN.blit(BACKGROUND_IMAGE, (0, 0))
+
+pygame.display.update()
+
+
+def main():
+
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
