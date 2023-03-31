2c654ee57f44a82380de0cd05434f422baba44c8
Pedro Kim <pedroh.kim00@gmail.com>
2023-03-31 00:38:45 -0300

    New PlayerShip

--- GameObjects/PlayerShip.py
+++ GameObjects/PlayerShip.py
@@ -1,14 +1,15 @@
-import GameObject, pygame, Projectile
-
+import pygame
+from GameObjects.GameObject import GameObject
+from GameObjects.Projectile import Projectile
 
 class PlayerShip(GameObject):
     def __init__(self, x, y, image):
-        super().__init__(x, y, image)
         self.width = 55
         self.height = 40
         self.bullets = []
         self.health = 10
         self.vel = 5
+        super().__init__(x, y, self.width, self.height, image)
 
     def move(self, keys_pressed, screen_border):
         if keys_pressed[pygame.K_w] and self.y - self.vel > 0:  # Move up
@@ -17,19 +18,22 @@ class PlayerShip(GameObject):
             self.x -= self.vel
         if (
             keys_pressed[pygame.K_s]
-            and self.y + self.height + self.vel < screen_border.y
+            and self.y + self.height + self.vel < screen_border.height
         ):  # Move down
             self.y += self.vel
         if (
             keys_pressed[pygame.K_d]
-            and self.x + self.width + self.vel < screen_border.x
+            and self.x + self.width + self.vel < screen_border.width
         ):  # Move right
             self.x += self.vel
 
-    def shoot(self, key_pressed):
-        if key_pressed[pygame.K_SPACE]:
-            bullet = Projectile(self.x + self.width / 2 - 5, self.y + self.height / 2)
-            self.bullets.append(bullet)
+    def shoot(self, surface):
+        #if key_pressed[pygame.K_SPACE]:
+        bullet = Projectile(self.x + self.width / 2 - 5, self.y + self.height / 2)
+        
+        self.bullets.append(bullet)
+        for bull in self.bullets:
+            bull.draw(surface)
 
 
     #def collide(self):
