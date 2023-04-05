c9eafe038bbb289f4200c7ffb041595c6c42d79d
Marcelo Melo <marcelo.moliveira@ga.ita.br>
2023-04-05 01:36:24 -0300

    add music and pause/despause sprite

--- kim_prototype.py
+++ kim_prototype.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, os
 import numpy as np
 from classes.PlayerShip import PlayerShip
 from classes.EnemyShip import EnemyShip
@@ -11,6 +11,12 @@ from constants.velocities import velocities
 from constants.dimensions import width, height
 from constants.images import planet_background_img, space_background_img, iss_img, spaceships_img, meteors_img
 
+# Initialize the mixer module
+pygame.mixer.init()  
+pygame.mixer.music.load(os.path.join("music", "main_music.mp3"))  # Load the MP3 file
+pygame.mixer.music.play(-1)  # Play the music
+
+# Initialize global variables
 WIN = pygame.display.set_mode((width.get("screen"), height.get("screen")))
 pygame.display.set_caption("ISS Defense")
 
@@ -27,21 +33,7 @@ def iss_start(space_station):
     if space_station.y < height.get("screen"):
         space_station.y += velocities.get("iss")
 
-# def handle_enemy_movement(enemy_ship, bullets):
-#     enemy_ship.y += ENEMY_SHIP_VEL
-#     for bullet in bullets:
-#         if enemy_ship.colliderect(bullet):
-            
-# def new_player_health(player_ship, player_health, enemy_bullets, meteors):
-#     for bullet in enemy_bullets:
-#         if player_ship.colliderect(bullet):
-#             player_health -= 1
-#     for meteor in meteors:
-#         if player_ship.colliderect(meteor):
-#             player_health -= 3
-#     return player_health
-
-def draw_window(PLANET_TIME,planet_frame,current_time,space_station, player, enemy, meteors, button_pause):
+def draw_window(PLANET_TIME, planet_frame,current_time,space_station, player, enemy, meteors):
     WIN.blit(space_background_img.get("space_background"), (0, 0))
 
     if current_time - PLANET_TIME > FPS:
@@ -55,10 +47,9 @@ def draw_window(PLANET_TIME,planet_frame,current_time,space_station, player, ene
         WIN.blit(planet_background_img.get("planet_background")[planet_frame[0]], (
             (width.get("screen") - width.get("planet"))/2,
             (height.get("screen") - height.get("planet"))/2))
-    #pygame.draw.rect(WIN, colors.get("white"), BORDER)
-    #WIN.blit(background_img.get("space_background"), (0, 0))
 
     WIN.blit(iss_img.get("first_iss"), (space_station.x, space_station.y))
+    
     for bullet in player.bullets:
             bullet.move()
             bullet.draw(WIN)
@@ -67,12 +58,17 @@ def draw_window(PLANET_TIME,planet_frame,current_time,space_station, player, ene
     for bullet in enemy.bullets:
         bullet.move()
         bullet.draw(WIN)
+    
     for meteor in meteors:
         meteor.draw(WIN)
 
     player.draw(WIN)
     enemy.draw(WIN)
-    button_pause.draw(WIN)
+    button_pause = pygame.transform.scale(
+        pygame.image.load(os.path.join("images", "tile_0266.png")),
+        (60, 60),
+    )
+    WIN.blit(button_pause, (10, 10))
 
     pygame.display.update()
 
@@ -96,31 +92,32 @@ def main():
 
     # player_health = 10
 
+    METEOR_TIME = 0
+    METEOR_TIME_DELAY = 500
+    PLANET_TIME = 0
+
     meteors = []
 
     planet_frame = [0]
 
     clock = pygame.time.Clock()
 
-    # ENEMY_SHOOT_TIME = 0
-    # ENEMY_SHOOT_DELAY = 1000
-
-    METEOR_TIME = 0
-    METEOR_TIME_DELAY = 500
-
-    PLANET_TIME = 0
-
-    ### Inicial Screens
     run = False
 
-    screenHistory = [0]
     pygame.init()
     clock.tick(20)
-    font = pygame.font.Font('RobotoMono-Light.ttf', 30)
-    button_play = Button(170, 100, 200, 50, colors.get("red"), "Jogar", font, (255, 255, 255))
-    button_instructions = Button(170, 200, 200, 50, colors.get("black"), "Instrucoes", font, (255, 255, 255))
-    button_history = Button(170, 300, 200, 50, colors.get("black"), "Historia", font, (255, 255, 255))
+    screenHistory = [START_SCREEN]
+    font = pygame.font.Font(os.path.join('fonts', 'RobotoMono-Light.ttf'), 30)
+    title_font = pygame.font.Font(os.path.join('fonts', 'RobotoMono-Regular.ttf'), 54)
+    button_title = Button(170, 100, 200, 90, colors.get("black"), "ISS Defense", title_font, (255, 255, 255))
+    button_play = Button(170, 300, 200, 50, colors.get("red"), "Jogar", font, (255, 255, 255))
+    button_instructions = Button(170, 400, 200, 50, colors.get("black"), "Instrucoes", font, (255, 255, 255))
+    button_history = Button(170, 500, 200, 50, colors.get("black"), "Historia", font, (255, 255, 255))
     button_return = Button(10, 10, 200, 50, colors.get("mustard"), "Retornar", font, (255, 255, 255))
+    button_pause = Button(10, 10, 50, 50, colors.get("mustard"), "Pausar", font, (255, 255, 255))
+    button_despause = Button(10, 10, 50, 50, colors.get("mustard"), "Despausar", font, (255, 255, 255))
+
+    esc_paused = False
 
     while not run:
         for event in pygame.event.get():
@@ -139,9 +136,10 @@ def main():
 
         if screenHistory[-1] is START_SCREEN:
             WIN.blit(space_background_img.get("space_background"), (0, 0))
+            button_title.draw(WIN)
             button_play.draw(WIN)
             button_instructions.draw(WIN) 
-            button_history.draw(WIN) 
+            button_history.draw(WIN)
         if screenHistory[-1] is INSTRUCTIONS_SCREEN:
             WIN.blit(space_background_img.get("space_background"), (0, 0))
             pygame.draw.rect(WIN, colors.get("white"), BORDER)
@@ -154,16 +152,12 @@ def main():
 
         pygame.display.update()
 
-    button_pause = Button(10, 10, 200, 50, colors.get("mustard"), "Pausar", font, (255, 255, 255))
-    button_despause = Button(10, 10, 200, 50, colors.get("mustard"), "Despausar", font, (255, 255, 255))
-
-    esc_paused = False
-
-    ### Game Screen
     while run:
         clock.tick(FPS)
         current_time = pygame.time.get_ticks()
         keys_pressed = pygame.key.get_pressed()
+
+            
 
         if current_time - METEOR_TIME > METEOR_TIME_DELAY:
             random_meteor_x = int((width.get("screen") - width.get("meteor")) * np.random.random())
@@ -207,7 +201,7 @@ def main():
                             run = False
                             pygame.quit()
                             sys.exit()
-                    button_despause.draw(WIN)
+                    WIN.blit(pygame.transform.scale(pygame.image.load(os.path.join("images", "tile_0265.png")),(60, 60),), (10, 10))
                     pygame.display.update()
 
 
@@ -221,7 +215,7 @@ def main():
             run = False
             main()
 
-        draw_window(PLANET_TIME,planet_frame,current_time,iss, player, enemy, meteors, button_pause)
+        draw_window(PLANET_TIME, planet_frame,current_time,iss, player, enemy, meteors)
         pygame.display.update()
 
 
